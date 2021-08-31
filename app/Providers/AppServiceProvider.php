0dd54830b6ad03b1155b2ad3bf08d6955ff8529c
Ryosuke <sessyomaru427@gmail.com>
2021-08-31 18:03:05 +0900

    httpsでも動くように

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('http');
+        if (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('https', true);
+        }
+
+        Schema::defaultStringLength(191);
     }
 }
